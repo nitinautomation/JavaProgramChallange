948ea8fc30a3d3ee9169a89f4718b9b47ba31232
GS-3287 <GS-3287@GS-5495.GSLAB.COM>
2023-02-28 10:18:52 +0530

    Commiting initial Changes

---
+++ TestProj/src/com/testpractice/ReverseString.java
@@ -0,0 +1,41 @@
+package com.testpractice;
+
+import java.util.Scanner;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+
+		System.out.println("Please Enter a String");
+		Scanner sc = new Scanner(System.in);
+
+		reverseStringWithoutSpecialChar(sc.nextLine());
+	}
+
+	private static void reverseStringWithoutSpecialChar(String inputString) {
+
+		char[] charArr = inputString.toCharArray();
+		int left = 0;
+		int right = charArr.length - 1;
+		while (left < right) {
+
+			if (!Character.isLetterOrDigit(charArr[left])) {
+				left++;
+			}
+			if (!Character.isLetterOrDigit(charArr[right])) {
+				right--;
+			} else {
+				char temp = charArr[left];
+				charArr[left] = charArr[right];
+				charArr[right] = temp;
+				left++;
+				right--;
+
+			}
+
+		}
+		System.out.println(charArr);
+
+	}
+
+}
